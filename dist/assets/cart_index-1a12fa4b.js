a0a08ad68e5bc9d1ee2b7b239469ab67883ad3a8
MKTA Web Developer <mktaanalytics@gmail.com>
2023-11-29 10:29:07 +0800

    This is a Sample Push

---
+++ dist/assets/cart_index-1a12fa4b.js
@@ -0,0 +1,1 @@
+import{_ as t,t as r}from"./index-b1207d8a.js";const s={emits:[],components:{},props:{},data(){return{userToken:localStorage.getItem("userToken")}},setup(){},created(){},mounted(){},methods:{}};function o(n,a,c,p,e,i){return" this is the cart "+r(e.userToken)}const _=t(s,[["render",o]]);export{_ as default};
